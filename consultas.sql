b1eed731d392aada56e301ebb3033059ab0e8400
Vitor Miolo <vitor.miolo@gestaods.com.br>
2023-11-27 23:25:49 -0300

    o que cada um fazer

--- consultas.sql
+++ consultas.sql
@@ -17,3 +17,18 @@ select u.nome, e.valor_estadia, dh.num_dias from fat_estadia e
 inner join dim_user u on e.dim_user_id = u.id
 inner join dim_hospedagem dh on e.dim_hospedagem_id = dh.id
 where dh.num_dias > 5;
+
+-- 1 Timas
+-- - Número de hospedagens em um determinado ano por tipo de propriedade
+-- - Número de hospedagens em um determinado ano com crianças
+
+-- 2 Hirrua
+-- - Média do número de dias de hospedagens
+-- - Média dos valores pagos pelas hospedagens
+
+-- 3 Vitor
+-- - Cliente com o maior valor já pago em uma hospedagem
+-- - Criar uma 
+
+-- 4 Gabriel
+-- - Criar duas
